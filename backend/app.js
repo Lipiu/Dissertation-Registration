f45795a6c04c0eb932b323fba7ae7dc2a1cc18c5
Rosu Liviu <rosuliviu10@gmail.com>
2025-12-28 22:23:28 +0200

    fixed bugs in accountRoutes

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,27 @@
 import express from 'express';
 import initDatabase from './config/initdb.js';
-
-const PORT = 8888;
+import accountRoutes from './routes/accountRoutes.js';
+import { PORT } from './config/const.js';
 
 const app = express();
+
+// middleware
 app.use(express.json());
 
+// init database
 (async () => {
 	try {
 		await initDatabase();
 		console.log('Database initialized');
-	} catch (err) {
-		console.error('Database init failed:', err);
+	} catch (error) {
+		console.error('Database initialization failed:', error);
 	}
 })();
 
+// routes testing
+app.use('/account', accountRoutes);
+
+// start server
 app.listen(PORT, () => {
-	console.log('Server running on port 3000');
+	console.log(`Server running at http://localhost:${PORT}`);
 });
--- backend/routes/accountRoutes.js
+++ backend/routes/accountRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import { JWT_KEY, MIN_PASS_LENGTH } from '../config/const.js';
 import { createUser, getUserByEmailAndCheckPassword } from '../model/user.js';
 import professor from '../model/professor.js';
@@ -7,7 +8,7 @@ import student from '../model/student.js';
 
 const accountRoutes = express.Router();
 
-async function generateToken(user, userModel){
+function generateToken(user, userModel){
     return jwt.sign(
         {
             userId: user.professorId || user.studentId,
@@ -22,8 +23,8 @@ async function generateToken(user, userModel){
     );
 }
 
-async function loginHandler(req, res, userModel){
-    const { name, email, password, repeatedPassword } = req.body;
+async function registerHandler(req, res, userModel){
+    let { name, email, password, repeatedPassword } = req.body;
 
     if(!name || !email || !password || !repeatedPassword){
         return res.status(400).json({
@@ -37,30 +38,60 @@ async function loginHandler(req, res, userModel){
         });
     }
 
-    if(password.lenth < MIN_PASS_LENGTH){
+    if(password.length < MIN_PASS_LENGTH){
         return res.status(400).json({
             message: 'Password must be at least 8 characters'
         });
     }
 
+    //hashing the password
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+
 
     try{
-        const user = await createUser(
-            userModel,
-            name,
-            email,
-            password
-        );
+        const user = await createUser(userModel, {name,email,password});
         return res.status(201).json(user);
     }
     catch(error){
-        res.status(500).json(e.message);
+        res.status(500).json(error.message);
+    }
+}
+
+async function loginHandler(req,res,userModel){
+    const { email, password } = req.body;
+
+    if(!email || !password){
+        return res.status(401).json({
+            message: 'Missing email or password'
+        });
+    }
+    try{
+        const user = await getUserByEmailAndCheckPassword(
+            userModel,
+            email,
+            password
+        );
+        const token = generateToken(user, userModel);
+        return res.status(200).json({
+            user,
+            token
+        });
+    }
+    catch(error){
+        return res.status(401).json({
+            message: error.message
+        });
     }
 }
 
 //register
 accountRoutes.post('/student/register', (req,res) => 
-    registerHandler
+    registerHandler(req,res,student)
+);
+
+accountRoutes.post('/professor/register', (req,res) => 
+    registerHandler(req,res,professor)
 );
 
 //login
